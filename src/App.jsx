997219f076b0edbd1d40224622a4b0d643ca583d
Adedamola Ajasa <azeemajasa@gmail.com>
2023-09-12 19:07:47 +0100

    navbar for wider screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import Home from "./pages/Home"
+import Navbar from "./layout/Navbar"
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <div>
+      <AnimatedCursor innerSize={20} color='39, 217, 186' />
+      <Navbar/>
       <Home/>
     </div>
     
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import  logo from '../assets/images/logo.png'
+import {Link} from 'react-router-dom'
+
+function Navbar(){
+    const[nav, setNav] = useState(false)
+    function handleClick(){
+        setNav(!nav)
+    }
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-#0a192f'>
+        <div className='ml-0 bg-left-top hover:scale-100 duration-75 overflow-hidden hover:bg-white '>
+        <a href="#home" alt='return home'>
+            <img src={logo} alt='logo' className='max-w-[100px]'/>
+        </a>
+        </div>
+
+        <ul className='hidden md:flex md:items-center space-x-10 cursor-pointer'>  
+            <li className='hover:text-white'>Home</li>
+            <li className='hover:text-white'>About</li>
+            <li className='hover:text-white'>Skills</li>
+            <li className='hover:text-white'>Contact</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
