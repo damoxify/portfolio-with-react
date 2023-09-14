5d41b5f9a61fbf1f64bfaa7b0e8242f8d4bc7000
Adedamola Ajasa <azeemajasa@gmail.com>
2023-09-14 14:42:27 +0100

    README updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+  import React from 'react'
 
-function Home(){
-  return (
-    <div name= 'home' className='w-full h-screen bg-slate-600'>
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <h1 className='text-2xl text-black-800 font-bold'>Hi, my name is</h1>
-        <h1 className='text-4xl sm:text-7xl  font-bold text-[#b6c0e0]'>Adedamola Ajasa,</h1>
-        <h2 className='text-4xl sm:text-7xl  font-bold text-[#8892b0]'>I'm a Frontend developer.</h2>
-        <p className='text-black py-8 max-w-[700px]'>I'm a frontend developer specializing in building (and occasionally
-            designing ) exceptional digital experiences. Currently, I'm focused on
-             building responsive full-stack web applications.</p>
+  function Home(){
+    return (
+      <div name= 'home' className='w-full h-screen bg-slate-600'>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+          <h1 className='text-2xl text-black-800 font-bold'>Hi, my name is</h1>
+          <h1 className='text-4xl sm:text-7xl  font-bold text-[#b6c0e0]'>Adedamola Ajasa,</h1>
+          <h2 className='text-4xl sm:text-7xl  font-bold text-[#8892b0]'>I'm a Frontend developer.</h2>
+          <p className='text-black py-8 max-w-[700px]'>I'm a frontend developer specializing in building (and occasionally
+              designing ) exceptional digital experiences. Currently, I'm focused on
+              building responsive full-stack web applications.</p>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-export default Home
+  export default Home
